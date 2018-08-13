680dc27b4214b277798ad7e3c67850ab7dcffd54
Roman Lobanov <s3399752@w8408186.cs.rmit.edu.au>
2018-08-13 18:48:15 +1000

    Commit from RMIT 13/08

---
+++ app/src/main/java/com/example/s3399752/foodtrucker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.s3399752.foodtrucker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+//http://izvornikod.com/Blog/tabid/82/EntryId/8/Creating-Android-button-with-image-and-text-using-relative-layout.aspx
