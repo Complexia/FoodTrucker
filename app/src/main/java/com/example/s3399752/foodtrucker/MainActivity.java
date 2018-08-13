2a6c4377ed61ae93d58ec88a8035cd4bcc330203
Roman Lobanov <s3399752@w8408186.cs.rmit.edu.au>
2018-08-13 19:31:41 +1000

    Second commit from RMIT 13/08

--- app/src/main/java/com/example/s3399752/foodtrucker/MainActivity.java
+++ app/src/main/java/com/example/s3399752/foodtrucker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.s3399752.foodtrucker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Activity1.class));
+            }
+        });
     }
 }
 //http://izvornikod.com/Blog/tabid/82/EntryId/8/Creating-Android-button-with-image-and-text-using-relative-layout.aspx
