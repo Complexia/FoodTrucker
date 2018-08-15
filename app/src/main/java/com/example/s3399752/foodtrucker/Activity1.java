4d638e5e9e2e8c8588bd93e6bc63b3090809d432
Roman Lobanov <s3399752@w8408160.cs.rmit.edu.au>
2018-08-15 18:31:22 +1000

    commit dynamic buttons 14/08

--- app/src/main/java/com/example/s3399752/foodtrucker/Activity1.java
+++ app/src/main/java/com/example/s3399752/foodtrucker/Activity1.java
@@ -1,7 +1,16 @@
 package com.example.s3399752.foodtrucker;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Activity1 extends AppCompatActivity {
 
@@ -9,5 +18,41 @@ public class Activity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1);
+
+        ArrayList<Button> buttons = new ArrayList<Button>();
+        LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        for(int i=0;i<30;i++){
+            Button button = (Button)inflater.inflate(R.layout.buttons,null);
+            button.setId(i+1);
+            button.setText("Hello" + (i+1));
+            button.setHeight(200);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(new Intent(Activity1.this,MainActivity.class
+                    ));
+                }
+            });
+            buttons.add(button);
+        }
+
+        LinearLayout layout = findViewById(R.id.layoutLinear);
+        for(int i=0;i<buttons.size();i++){
+            layout.addView(buttons.get(i));
+            TextView text = (TextView)inflater.inflate(R.layout.texts,null);
+            text.setText("world");
+            layout.addView(text);
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/s3399752/foodtrucker/MainActivity.java
+++ app/src/main/java/com/example/s3399752/foodtrucker/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.s3399752.foodtrucker;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button1 = findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        Button btn1 = findViewById(R.id.button1);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Activity1.class));
             }
         });
+
+
     }
 }
 //http://izvornikod.com/Blog/tabid/82/EntryId/8/Creating-Android-button-with-image-and-text-using-relative-layout.aspx
